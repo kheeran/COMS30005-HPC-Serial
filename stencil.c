d8abe15a9cd0530ca6dd3494f7d41f4176dcb726
Kheeran <kn16063@my.bristol.ac.uk>
2018-10-25 01:54:53 +0100

    relogging gprof

--- stencil.c
+++ stencil.c
@@ -49,8 +49,8 @@ int main(int argc, char *argv[]) {
 }
 
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
-  for (int i = 0; i < ny; ++i) {
-    for (int j = 0; j < nx; ++j) {
+  for (int j = 0; j < ny; ++j) {
+    for (int i = 0; i < nx; ++i) {
       tmp_image[j+i*ny] = image[j+i*ny] * 3.0/5.0;
       if (i > 0)    tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.5/5.0;
       if (i < nx-1) tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.5/5.0;
