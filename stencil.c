e40d1172ffe6aa251ac9b029825df7e7089502a2
Kheeran <kn16063@my.bristol.ac.uk>
2018-10-25 02:18:18 +0100

    manually changing nx & ny in code

--- stencil.c
+++ stencil.c
@@ -19,8 +19,10 @@ int main(int argc, char *argv[]) {
   }
 
   // Initiliase problem dimensions from command line arguments
-  int nx = atoi(argv[1]);
-  int ny = atoi(argv[2]);
+  // int nx = atoi(argv[1]);
+  int nx = 1024
+  int ny = nx
+  // int ny = atoi(argv[2]);
   int niters = atoi(argv[3]);
 
   // Allocate the image
